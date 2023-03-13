785155a759cd74c1461954a9fecb6d7e1da39637
Agustin Lavarello <agustinlare@gmail.com>
2023-03-13 18:15:51 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+	asd = random.randint(1,70)
+
+	if request.method == 'GET':
+		return 'Esta es una prueba'
+	
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",port=8080)
