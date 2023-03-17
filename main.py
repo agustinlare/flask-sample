8c07b10952df266bb1b7a08f8b9a0bee394d97f4
Agustin Lavarello <agustinlare@gmail.com>
2023-03-17 09:53:29 -0300

    final commit

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	asd = random.randint(1,70)
-
-	if request.method == 'GET':
-		return 'Esta es una pruebaaaa'
+	return 'Hello, Galicia!'
 	
 if __name__ == '__main__':
 	app.run(host="0.0.0.0",port=8080)
